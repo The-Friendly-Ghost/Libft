8a75d603846d4b0af7e1030ede30b9141d3b9821
Casper Post <github-contact@capo.mozmail.com>
2022-10-25 11:20:04 +0200

    Added ft_nulloc

---
+++ src/ft_nulloc.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_nulloc.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cpost <cpost@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/25 11:10:08 by cpost         #+#    #+#                 */
+/*   Updated: 2022/10/25 11:18:43 by cpost         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * @brief Creates a new array filled with NULL pointers.
+ * @param n_point The amount of NULL pointers to create.
+ * @return Pointer to the newly created array
+ * @note
+ */
+char	*ft_nulloc(size_t n_point)
+{
+	char	*ptr;
+	int		i;
+
+	ptr = malloc(n_point * sizeof(char *));
+	if (ptr)
+	{
+		i = 0;
+		while (i < n_point)
+			ptr[i++] = NULL;
+	}
+	return (ptr);
+}
